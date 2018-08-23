2adef3a3345faca794fc2f75b03d1e12227ccfc6
Jesse Zelaya <jesse@seedcms.com>
2018-08-23 16:43:43 -0700

    Lazysizes install
    Set up lazysizes with reusable lazy image snippets

---
+++ src/scripts/lazysizes.js
@@ -0,0 +1,5 @@
+/*================ Lazy Loading ================*/
+// =require vendor/lazysizes/ls.rias.min.js
+// =require vendor/lazysizes/ls.parent-fit.min.js
+// =require vendor/lazysizes/ls.bgset.min.js
+// =require vendor/lazysizes/lazysizes.min.js
